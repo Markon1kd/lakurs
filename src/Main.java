1e97d21ba4eb57b6c1962315a2f64e4c80e06d08
Markon1kd <alexsandar99@mail.ru>
2018-12-16 11:00:35 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!`");
-        System.out.println("Second Commit")
+        System.out.println("Second Commit");
+
+        System.out.println("Changes on IDE Intellij Idea");
     }
 }
